2c74b4e7f6eed4bf8f00633aa34186c82866ff96
devilgupta <57756025+devilgupta@users.noreply.github.com>
2021-10-29 17:25:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/Home';
+import {SafeAreaView} from "react-native-safe-area-context"
+export default function App() {
+  <SafeAreaView>
+  return <HomeScreen/>
+  </SafeAreaView>
+}
